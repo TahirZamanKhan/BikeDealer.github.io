61697b32e1b69411681a06b3417c9e576ec219cd
TahirZamanKhan <tahirzamankhan2002@gmail.com>
2023-06-03 11:57:37 +0500

    Add project files.

---
+++ vroom/Migrations/20230519071033_AddModelandMaketoDb.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace vroom.Migrations
+{
+    public partial class AddModelandMaketoDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Bikes",
+                newName: "Id");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Currency",
+                table: "Bikes",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 10,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Bikes",
+                newName: "ID");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Currency",
+                table: "Bikes",
+                maxLength: 10,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 10);
+        }
+    }
+}
